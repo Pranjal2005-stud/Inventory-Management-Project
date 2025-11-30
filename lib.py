24b845df2783092609d5dba2569b0f2389daf189
Pranjal Raghuwanshi <raghuwanshipranjal9@gmail.com>
2025-11-30 19:09:20 +0530

    Implement calculate_price function in lib.py
    Add legacy price calculation function.

---
+++ lib.py
@@ -0,0 +1,7 @@
+def calculate_price(unit_price, quantity):
+    """
+    Calculate total price based on unit price and quantity.
+    This is the legacy business logic that must be used.
+    """
+    
+    return unit_price * quantity
